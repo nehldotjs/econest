8a1c85339aca612aa9f7299583c98484827a09b7
nehldotjs <nelsonosuya11@gmail.com>
2025-04-24 02:36:12 +0100

    new commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/econest/">
+  <BrowserRouter basename="/econest">
     <App />
   </BrowserRouter>
 );
